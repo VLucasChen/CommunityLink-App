8ca210caf5b4923c9ba425d64c1bec05f2cf4cef
Le Hoang <lhoa0020@student.monash.edu>
2025-11-07 16:12:13 +0700

    add new funtion for crud

--- src/Controller/OrganisationsController.php
+++ src/Controller/OrganisationsController.php
@@ -3,98 +3,106 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-/**
- * Organisations Controller
- *
- * @property \App\Model\Table\OrganisationsTable $Organisations
- */
+use Cake\Http\Response;
+
 class OrganisationsController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
     public function index()
     {
-        $query = $this->Organisations->find();
-        $organisations = $this->paginate($query);
-
+        $this->paginate = [
+            'contain' => [],
+            'order' => ['Organisations.org_name' => 'ASC'],
+            'limit' => 15
+        ];
+        $organisations = $this->paginate($this->Organisations);
         $this->set(compact('organisations'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Organisation id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function view($id = null)
     {
         $organisation = $this->Organisations->get($id, contain: ['Events']);
         $this->set(compact('organisation'));
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
-        $organisation = $this->Organisations->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $organisation = $this->Organisations->patchEntity($organisation, $this->request->getData());
-            if ($this->Organisations->save($organisation)) {
-                $this->Flash->success(__('The organisation has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The organisation could not be saved. Please, try again.'));
-        }
-        $this->set(compact('organisation'));
+        return $this->handleForm();
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Organisation id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function edit($id = null)
     {
-        $organisation = $this->Organisations->get($id, contain: []);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $organisation = $this->Organisations->patchEntity($organisation, $this->request->getData());
-            if ($this->Organisations->save($organisation)) {
-                $this->Flash->success(__('The organisation has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The organisation could not be saved. Please, try again.'));
-        }
-        $this->set(compact('organisation'));
+        $organisation = $this->Organisations->get($id);
+        return $this->handleForm($organisation);
     }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Organisation id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
+    public function delete($id = null): ?Response
     {
         $this->request->allowMethod(['post', 'delete']);
         $organisation = $this->Organisations->get($id);
+
         if ($this->Organisations->delete($organisation)) {
-            $this->Flash->success(__('The organisation has been deleted.'));
+            $this->Flash->success('Organisation has been deleted.');
         } else {
-            $this->Flash->error(__('The organisation could not be deleted. Please, try again.'));
+            $this->Flash->error('Unable to delete organisation.');
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    // ========== MODAL CONTENT ==========
+    public function modal(): ?Response
+    {
+        $this->viewBuilder()->setLayout('ajax');
+
+        $mode = $this->request->getQuery('mode');
+        $id = $this->request->getQuery('id');
+
+        $organisation = match ($mode) {
+            'add' => $this->Organisations->newEmptyEntity(),
+            'edit', 'view' => $this->Organisations->get($id, contain: ['Events']),
+            default => throw new \Cake\Http\Exception\NotFoundException()
+        };
+
+        $this->set(compact('organisation', 'mode'));
+        return $this->render($mode === 'view' ? 'view_modal' : 'form_modal');
+    }
+
+    // ========== PRIVATE: HANDLE ADD & EDIT ==========
+    private function handleForm($organisation = null): ?Response
+    {
+        if (!$organisation) {
+            $organisation = $this->Organisations->newEmptyEntity();
+        }
+
+        if ($this->request->is(['post', 'put'])) {
+            $organisation = $this->Organisations->patchEntity($organisation, $this->request->getData());
+
+            if ($this->Organisations->save($organisation)) {
+                if ($this->request->is('ajax')) {
+                    return $this->response
+                        ->withType('json')
+                        ->withStringBody(json_encode(['success' => true]));
+                }
+                $this->Flash->success('Organisation has been saved.');
+                return $this->redirect(['action' => 'index']);
+            }
+
+            if ($this->request->is('ajax')) {
+                $this->set(compact('organisation'));
+                $this->viewBuilder()->setTemplate('form_modal');
+                $html = $this->render()->getBody()->__toString();
+                return $this->response
+                    ->withType('json')
+                    ->withStringBody(json_encode([
+                        'success' => false,
+                        'html' => $html
+                    ]));
+            }
+
+            $this->Flash->error('Please fix the errors below.');
+        }
+
+        $this->set(compact('organisation'));
+        return null;
     }
 }
